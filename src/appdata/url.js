33a404703dcbb772522609c46a1fc5fa4ff125d4
Matuki92 <matiaspierretti2@hotmail.com>
2019-02-08 21:28:11 +0100

    testing api urls

--- src/appdata/url.js
+++ src/appdata/url.js
@@ -1,4 +1,4 @@
 
-export const API_URL = 'http://localhost:3000',
-  SOCKET_URL = 'http://localhost:3001',
+export const API_URL = 'dev.tapman.beer:3000',
+  SOCKET_URL = 'dev.tapman.beer:3001',
   appLogo = 'http://guma.tapman.beer/images/tapmanlogo-small.png';
